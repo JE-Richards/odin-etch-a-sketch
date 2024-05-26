1e2de8609a2e018fb361e8984eb27fac52471ee8
Jon-Ethan Richards <45516368+JE-Richards@users.noreply.github.com>
2024-05-26 13:12:43 +0100

    Add random grid cell color change on event
    Modified the event listener for each cell in the grid to randomise the
    RGB colour selection upon each mouse enter. This is to satisfy one of
    the "extra credit" activities in the TOP project exercise.
    
    Additionally added very basic styling to the button and header on the
    webpage to make everything appear centered on the screen.

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ body.appendChild(container);
 
 gridBttn.addEventListener("click", bttnClick);
 
-// Define a function to get the user input to select the size of the sketchpad grid.
-// Function will need to ensure the input is a number between 1 and 100, and handle edge cases such as trailing spaces
+// Define a function to get the user input to select the size of the sketchpad grid.;
+// Function will need to ensure the input is a number between 1 and 100, and handle edge cases such as trailing spaces;
 function getUserInput() {
     let gridPrompt = "To select the number of rows and columns in your sketchpad, please enter a number " +
     "between 1 and 100:";
@@ -30,9 +30,13 @@ function getUserInput() {
 
 // Want to avoid duplicate code, so use nested for loops to create our grid;
 // Used row + column boxes in order to ensure each row has the correct number of boxes;
-// Alternative would be to not define the number in a row and instead make boxes directly into container and let 
+// Alternative would be to not define the number in a row and instead make boxes directly into container and let;
 // the container flex-wrap - but this doesn't guarantee a fixed number of elements per row;
 function createGrid(userInput) {
+
+    //remove the grid if a grid already exists;
+    container.innerHTML = "";
+
     for (i=1; i<=userInput; i++) {
         let row = document.createElement("div");
         row.classList.add("row");
@@ -42,7 +46,11 @@ function createGrid(userInput) {
             row.appendChild(box);
             box.addEventListener("mouseenter", color);
             function color () {
-                box.style.backgroundColor = "black";
+                // select a random rgb value for the box upon every mouseenter;
+                let r = Math.floor(Math.random() * 255);
+                let g = Math.floor(Math.random() * 255);
+                let b = Math.floor(Math.random() * 255);
+                box.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b +")";
             }
         }
         container.appendChild(row);
@@ -55,8 +63,8 @@ function bttnClick() {
     createGrid(userInput);
 }
 
-// Modifying the CSS stylesheet via JS
+// Modifying the CSS stylesheet via JS;
 style.insertRule("body {margin: 0; box-sizing: border-box;}")
-style.insertRule(".container {max-width: 1200px; max-height: 1200px; width: 100%; margin: 10px; display: flex; flex-direction: column;}");
-style.insertRule(".row {display: flex; flex:1; width: 100%;}")
+style.insertRule(".container {max-width: 1200px; max-height: 1200px; width: 100%; margin: 10px; display: flex; flex-direction: column; justify-content: center; align-items: center;}");
+style.insertRule(".row {display: flex; flex:1; width: 100%; justify-content: center}")
 style.insertRule(".box {min-height: 5px; min-width: 5px; height: 10px; width: 10px; background-color: white; flex-shrink: 1; border: 1px solid black;}");
